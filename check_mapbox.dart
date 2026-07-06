02a6d7fa776708bb30dca81b21a496862ef503a4
Junho73 <2471521@hansung.ac.kr>
2026-07-06 13:52:46 +0900

    check_mapbox.dart 파일의 누락된 geometry 파라미터 에러 수정

--- check_mapbox.dart
+++ check_mapbox.dart
@@ -3,7 +3,9 @@ import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 void main() {
   PointAnnotationOptions opts = PointAnnotationOptions(
-    image: Uint8List(0), // Corrected to Uint8List
+    geometry: Point(coordinates: Position(0.0, 0.0)),
+    image: Uint8List(0),
     iconImage: 'string',
   );
+  print(opts);
 }
